7bd242ce90b22ed1e77b1260edfd521a75185206
melih kocaturk <melih.kocaturk@anextour.com>
2024-04-17 20:41:47 +0300

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if (config('app.env') === 'production') {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
 

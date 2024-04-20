a5c481d977f3e60d3c3ecafbea327f7d062209a5
melih kocaturk <melih.kocaturk@anextour.com>
2024-04-20 15:26:54 +0300

    refactor

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,9 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
-{
-    protected $dateFormat = 'd/m/Y';
-    
+{   
     use HasFactory;
 
     protected $fillable = [
